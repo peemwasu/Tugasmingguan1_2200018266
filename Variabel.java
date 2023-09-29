e2db2e748c681e1535054ae41b8526ad88a461de
Peemwasu <widyafahmidasari@gmail.com>
2023-09-29 20:33:41 +0700

    Program Variabel

---
+++ Variabel.java
@@ -0,0 +1,16 @@
+public class Variabel {
+    public static void main(String[] args){
+//  Membuat variabel
+        String nama;
+        int usia;
+        double nilai;
+
+        nama = "Alya";
+        usia = 20;
+        nilai = 90.5;
+
+        System.out.println("Nama  : " + nama);
+        System.out.println("Usia  : " + usia);
+        System.out.println("Nilai : " + nilai);
+    }
+}
