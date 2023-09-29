5bd5b2e28195b994d15b5118c7c0b5041c403afc
Peemwasu <widyafahmidasari@gmail.com>
2023-09-29 20:02:11 +0700

    Commit Pertama

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+//      Menampilkan kata menggunakan fungsi println
+        System.out.println("Hello");
+        System.out.println("World");
+//      Meanmpilkan kata menggunakan fungsi print
+        System.out.print("Hello");
+        System.out.print(" World");
+    }
+}
