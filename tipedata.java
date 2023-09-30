cc883a5eca703201a7f37481324815efdb882dbe
Peemwasu <widyafahmidasari@gmail.com>
2023-09-30 14:29:49 +0700

    Pengenalan Tipe Data

---
+++ tipedata.java
@@ -0,0 +1,27 @@
+public class tipedata {
+    public static void main(String[] args){
+        char golongandarah; // tipe data char
+        int umur; // tipe data angka
+        float nilai; // tipe data angka dengan koma
+        double nilairatarata; // tipe data angka desimal
+        String nama; // tipe data untuk teks
+        boolean valid; // tipe data nilai true dan false
+
+        nama = "Widya";
+        golongandarah = 'A';
+        nilai = 95;
+        umur = 20;
+        nilairatarata = 87.77543;
+        valid = true;
+
+        System.out.println("Nama            : " + nama);
+        System.out.println("Umur            : " + umur);
+        System.out.println("GolDar          : " + golongandarah);
+        System.out.println("Nilai           : " + nilai);
+        System.out.println("Nilai Rata Rata : " + nilairatarata);
+        System.out.println("Data Valid      : " + valid);
+
+
+
+    }
+}
