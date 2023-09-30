73118dd3a4b938294051113900f738bcffd8b5b2
Peemwasu <widyafahmidasari@gmail.com>
2023-09-30 19:41:12 +0700

    Do While Loop

---
+++ dowhile.java
@@ -0,0 +1,9 @@
+public class dowhile {
+    public static void main(String[] args){
+        int i = 1;
+        do {
+            System.out.println("Perulangan ke-" + i);
+            i++;
+        } while(i <= 10);
+    }
+}
