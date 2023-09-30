8caa75278a783f73786764ceb40ef1568be6bd3f
Peemwasu <widyafahmidasari@gmail.com>
2023-09-30 19:42:19 +0700

    Array 1 Dimensi

---
+++ array1dimensi.java
@@ -0,0 +1,8 @@
+public class array1dimensi {
+    public static void main(String[] args){
+     int[] harga = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1} ;
+        for(int i=0; i<= harga.length; i++){
+            System.out.println(harga[i]);
+        }
+    }
+}
