ccb9fc2a10f9c7c31a557c6a29413c6c8eb9c16c
Peemwasu <widyafahmidasari@gmail.com>
2023-09-30 14:56:18 +0700

    Pengenalan If Function

---
+++ iffunction.java
@@ -0,0 +1,11 @@
+public class iffunction {
+    public static void main(String[] args){
+        int angka=5;
+        if (angka/2 != 0) {
+            System.out.println(angka + " = Bilangan Ganjil");
+        }
+        else {
+            System.out.println(angka + " = Bilangan Genap");
+        }
+    }
+}
