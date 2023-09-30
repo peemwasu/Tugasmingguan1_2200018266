2593c7bb56dc3ea7ac010e1d55f142c7527496ec
Peemwasu <widyafahmidasari@gmail.com>
2023-09-30 14:48:34 +0700

    Perulangan For

---
+++ forloop.java
@@ -0,0 +1,14 @@
+public class forloop {
+    public static void main(String[] args){
+        for (int i=1; i <= 10; i++){
+            System.out.println("Tampilkan kalimat ke-" + i);
+        }
+
+        for (int i=1; i<=5; i++){
+            for (int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
